057df8f360c2b749be7d706fe09f82aa009abf10
cote2 <64438037+cote2@users.noreply.github.com>
2020-06-01 23:07:37 -0500

    trabajo de main

--- src/main/java/productoAdyacentes.java
+++ src/main/java/productoAdyacentes.java
@@ -3,8 +3,23 @@ import java.util.Scanner;
 public class productoAdyacentes {
 
     public static void main(String[] args){
-
+        
+        int[] num = creacionArreglo();
+        int mayor= productoAdyacentes(num);
+        System.out.println("El num mayor es: "+mayor);
     }
+    
+     public static int[] creacionArreglo (){
+            System.out.println("Ingrese cantidad de numeros que desee ingresar");
+            int cant = validar();
+            int[] arr = new int[cant];
+
+            for (int i=0; i<arr.length; i++) {
+                System.out.println("numero "+i);
+                arr[i]= validar();
+            }
+            return arr;
+        }
 
     public int productoAdyacentes(int [] arr) {
         int i;
